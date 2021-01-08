85964e7d624a41edcfbdb545dc15e9710208adf5
adidion19 <didionantoine@gmail.com>
2021-01-08 14:02:05 +0100

    08/01

--- ft_printf.c
+++ ft_printf.c
@@ -186,41 +186,54 @@ int			ft_atoi(const char *str)
 	return ((int)i);
 }
 
-char	*ft_tab_error(char s2)
+char	*ft_convert_d_2(int n, int i, t_list lst)
 {
 	char *str;
+	int j;
 
-	str[0] = s2;
-	return (str);
-}
-
-char	*ft_strcharjoin(char *s1, char s2)
-{
-	int i;
-	char *str;
-
-	i = -1;
-	if (!s1 && !s2)
+	str = 0;
+	if (i == 0)
 		return (0);
-	if (!s1)
-		return (ft_tab_error(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	str = malloc(sizeof(ft_strlen(s1) + 1) + 1);
-	while (s1[++i])
-		str[i] = s1[i];
-	str[++i] = s2;
-	str[i] = '\0';
+	if (n < 0)
+	{
+		str = ft_strjoin(str, "-");
+		n = -n;
+	}
+	j = ft_strlen(ft_itoa(n));
+	if (lst.t_flag == 0)
+	{
+		while ((i-- > lst.max) && (lst.min > j) && (lst.max > j))
+		{
+			str = ft_strjoin(str, " ");
+		}
+		while ((lst.max--) - j)
+		{
+			str = ft_strjoin(str, "0");
+		}
+	}
 	return (str);
 }
 
 char *ft_convert_d(char *av , t_list lst, va_list ap)
 {
     int n;
+	int i;
     char *str;
 
+	i = 0;
 	n = va_arg(ap, int);
-    str = ft_itoa(n);
+	if (lst.min >= lst.max)
+		i = lst.min - lst.max;
+	if (lst.t_flag == 0)
+	{
+		str = ft_convert_d_2(n, i, lst);
+    	str = ft_strjoin(str, ft_itoa(n));
+	}
+	else
+	{
+		str = ft_itoa(n);
+		str = ft_strjoin(str, ft_convert_d_2(n, i, lst));
+	}
     return (str);
 }
 
@@ -232,15 +245,30 @@ char *ft_flag(char **s, char *av , t_list lst, va_list ap)
     return (str);
 }
 
-void    ft_check_mm(char *av, t_list lst)
+void    ft_check_mm(char *av, t_list lst, va_list ap)
 {
     int i;
+	int n;
 
     i = 0;
     if (av[i] >= 1 && av[i] <= 9 && lst.period == 1)
         lst.max = ft_atoi(av);
     if (av[i] >= 1 && av[i] <= 9 && !lst.period)
         lst.min = ft_atoi(av);
+	if (lst.zero_flag == 1 && lst.t_flag == 1)
+		lst.zero_flag = 0;
+	if (av[i] == '*' && lst.period == 0)
+	{
+		n = va_arg(ap, int);
+		lst.min = n;
+	}
+	if (av[i] == '*' && lst.period == 1)
+	{
+		n = va_arg(ap, int);
+		lst.max = n;
+	}
+	if (lst.zero_flag == 1 && lst.period == 0 && lst.min)
+		lst.max = lst.min;
 }
 
 char *ft_lst_flag(char *s, char *av, va_list ap, t_list lst)
@@ -269,7 +297,7 @@ char *ft_lst_flag(char *s, char *av, va_list ap, t_list lst)
                 break;
             }
         }
-        ft_check_mm(((char*)(av)) + i, lst);
+        ft_check_mm(((char*)(av)) + i, lst, ap);
     }
     str = ft_flag(&s, ((char*)(av)) + i, lst, ap);
     return (str);
@@ -282,12 +310,12 @@ char *ft_lst_init(char *s,char *av, va_list ap)
 
 	lst.zero_flag = 0;
 	lst.t_flag = 0;
-	lst.max = INT_MAX;
+	lst.max = 0;
 	lst.min = 0;
 	lst.period = 0;
 	lst.flag = '\0';
 	lst.star1 = 0;
-	lst.star2 = INT_MAX;
+	lst.star2 = 0;
 	str = (ft_lst_flag(s,((char*)av), ap, lst));
 	return (str);
 }
@@ -299,12 +327,10 @@ int     ft_printf(const char *av, ...)
     va_start(ap, av);
     char *str;
 	char *s;
-	//int k;
     int j;
 
     i = 0;
     j = 0;
-	//k = ft_len_fin((char*)av, ap);
    while (av[i])
     {
         if (av[i] == '%')
@@ -316,17 +342,17 @@ int     ft_printf(const char *av, ...)
 		}
         else
 		{
-			//str = ft_strcharjoin(str, av[i]);
 			ft_putchar_fd(av[i], 1);
 			i++;
 			j++;
 		}
     }
     va_end(ap);
-    return ((int)ft_strlen(str)); //incrementer j
+    return (j);
 }
 
 int main()
 {
-    ft_printf("aaaa%daaa", 5);
+    ft_printf("aaaa%.10dna\n", 515);
+	printf("aaaa%.10dna\n", 515);
 }
